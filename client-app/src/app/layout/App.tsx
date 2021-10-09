1b1f9ff320e8913a6f8f2ded504aecdfda5f11c0
tanmaym150 <tanmaymahamulkar150@gmail.com>
2021-10-09 13:23:47 +0530

    End of Section 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,24 +1,35 @@
 import React, { Fragment, useEffect, useState } from 'react';
 
 
-import axios from 'axios';
+
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import LoadingComponent from './LoadingComponent';
 
 
 function App() {
 
- const [activities,setActivities]=useState<Activity[]>([]);
+ const[activities,setActivities]=useState<Activity[]>([]);
  const[selectedActivity,setSelectedActivity]=useState<Activity | undefined>(undefined);
  const[editMode,setEditMode]=useState(false);
+ const[loading,setLoading]=useState(true);
+ const [submitting,setSubmitting]=useState(false);
 
  useEffect(()=>{
-   axios.get<Activity[]>('http://localhost:5000/api/activities').then(response=>{
-     
-       setActivities(response.data);
+   agent.Activities.list().then(response=>{
+      let activities:Activity[]=[];
+      response.forEach(activity=>{
+        activity.date=activity.date.split('T')[0];
+        activities.push(activity);
+      })
+       setActivities(activities);
+       setLoading(false);
    })
  },[])
 
@@ -40,6 +51,21 @@ function App() {
  }
 
  function handleCreateOrEditActivity(activity:Activity){
+   setSubmitting(true);
+   if(activity.id){
+     agent.Activities.update(activity).then(()=>{
+       setActivities([...activities.filter(x=>x.id !== activity.id),activity])
+       setSelectedActivity(activity);
+       setEditMode(false);
+       setSubmitting(false);
+     })
+   }else{
+     activity.id=uuid();
+     agent.Activities.create(activity).then(()=>{
+       setActivities([...activities,activity])
+     })
+
+   }
   activity.id
    ? setActivities([...activities.filter(x=>x.id!==activity.id),activity])
   :setActivities([...activities,{...activity,id:uuid()}]);
@@ -48,10 +74,15 @@ function App() {
 
 }
 function handleDeleteActivity(id:string){
-  setActivities([...activities.filter(x=>x.id !== id)])
+  setSubmitting(true);
+  agent.Activities.delete(id).then(()=>{
+    setActivities([...activities.filter(x=>x.id !== id)])
+    setSubmitting(false);
+  })
+  
 }
 
-
+if (loading) return <LoadingComponent content='Loading app'/>
   return (
     <Fragment>
 
@@ -67,6 +98,8 @@ function handleDeleteActivity(id:string){
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
+
 
 
           />
